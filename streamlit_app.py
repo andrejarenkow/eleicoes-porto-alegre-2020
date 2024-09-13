8a8758474cae65b66a9e0e8510187d81eaf25967
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-09-13 16:52:59 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import requests
 import streamlit as st
 import pandas as pd
+import geopandas as gpd
+
+
 
 #mostrar todas colunas
 pd.set_option('display.max_columns', None)
@@ -10,9 +13,23 @@ pd.set_option('display.max_columns', None)
 def buscando_dados():
   df = pd.read_csv('votacao_secao_2020_RS_POA.zip', sep = ';')
 
-  return df
+  #load o arquivo geojson e transformar em geodataframe
+  gdf = gpd.read_file('/content/locais_votacao.json')[['_', 'geometry']]
 
-df = buscando_dados()
+  return df, gdf
 
-df
+df, gdf = buscando_dados()
 
+turno = st.selectbox('Selecione o turno', options = [1, 2])
+cargo = st.selectbox('Selecione o cargo', options = ['Vereador', 'Prefeito'])
+candidato = st.selectbox('Selecione o candidato', options = sorted(df['NM_VOTAVEL'].unique()))
+
+# filtro para município, prefeito e primeiro turno
+filtro = (df['DS_CARGO']==cargo)&(df['NR_TURNO']==turno)&(df['NM_VOTAVEL']==candidato)
+df_municipio = df[filtro].reset_index(drop=True)
+
+# Chave unica zona+secao
+df_municipio['chave'] = df_municipio['NR_ZONA'].astype(int).astype(str) + '-' + df_municipio['NR_SECAO'].astype(str)
+
+# Mostrando o df
+df_municipio
